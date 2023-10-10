516816d6534802171a6980feaabce30db6af91ba
Sriharsha6197 <m.sriharsha97@gmail.com>
2023-10-10 17:36:16 +0530

    changes

--- memory.sh
+++ memory.sh
@@ -1,6 +1,6 @@
 free
 memTotal=$(free | grep "Mem" | awk -F "      " '{print$2}' | xargs)
-memUsed=$(free | grep "Mem" | awk -F " " '{print$4}')
+memUsed=$(free | grep "Mem" | awk -F "      " '{print$4}' | xargs)
 
 echo $memTotal
 echo $memUsed
