d2bc071ecf48e758442ffc903893a8bd617ae94b
Sriharsha6197 <m.sriharsha97@gmail.com>
2023-10-10 18:20:24 +0530

    c

--- memory.sh
+++ memory.sh
@@ -11,3 +11,12 @@ echo $memFree
 
 freePercentage=$(echo "scale=2;$memFree / $memTotal * 100 " | bc -l)
 echo $freePercentage
+
+if ( $freePercentage -le 50);
+then
+echo "\e[31mRed\e[0m"
+elif ( $freePercentage -le 70);
+then
+echo "\e[33mYellow\e[0m"
+else
+echo "\e[31mGreen\e[0m"
